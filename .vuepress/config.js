3c878f07af4fb0aeb602fcdc3f6d325d580511b2
Kevin <ltwoggbuty2mh@gmail.com>
2023-10-04 19:19:07 +0800

    docs compress

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -26,6 +26,42 @@ module.exports = {
         ["meta", {name: "apple-mobile-web-app-capable", content: "yes"}]
     ],
     plugins: [
+        ['@vuepress/back-to-top', true],
+        ['@vuepress/medium-zoom', {
+            selector: 'img',
+            // See: https://github.com/francoischalifour/medium-zoom#options
+            options: {
+                margin: 16
+            }
+        }],
+        // see: https://vssue.js.org/guide/vuepress.html#usage
+        ['@vssue/vuepress-plugin-vssue', {
+            // set `platform` rather than `api`
+            platform: 'github',
+            // all other options of Vssue are allowed
+            owner: 'realpdai',
+            repo: 'tech-arch-doc-comments',
+            clientId: 'xxxxxxxxxxx',
+            clientSecret: 'xxxxxxxxxxxxxxxxxxxxxx',
+        }],
+        // see: https://vuepress.github.io/zh/plugins/copyright/#%E5%AE%89%E8%A3%85
+        ['copyright', {
+            noCopy: false, // 允许复制内容
+            minLength: 100, // 如果长度超过 100 个字符
+            authorName: "",
+            // clipboardComponent: "请注明文章出处, [Java 全栈知识体系]()"
+        }],
+        // see: https://github.com/ekoeryanto/vuepress-plugin-sitemap
+        ['sitemap', {
+            hostname: ''
+        }],
+        // see: https://github.com/IOriens/vuepress-plugin-baidu-autopush
+        ['vuepress-plugin-baidu-autopush', {
+
+        }],
+        // see: https://github.com/znicholasbrown/vuepress-plugin-code-copy
+        [['vuepress-plugin-code-copy', true]]
+        // ...
     ],
     themeConfig: {
         // repo: "realpdai/tech-arch-doc",
@@ -40,10 +76,43 @@ module.exports = {
                 editLinkText: "在 GitHub 上编辑此页",
                 lastUpdated: "上次更新",
                 nav: [
+                    {
+                        text: '关于', link: '/md/about/me/about-me.md'
+                    }
                 ],
                 sidebar: {
+                    "/md/about/": genSidebar4About()
                 }
             }
         }
     }
 };
+
+// about page
+function genSidebar4About(){
+    return [
+        {
+            title: "关于",
+            collapsable: false,
+            sidebarDepth: 0,
+            children: [
+                "me/about-me.md",
+                "me/about-content.md",
+                "me/about-content-style.md",
+                "me/about-arch.md",
+                "me/about-motivation.md"
+            ]
+        },
+        {
+            title: "关于 - 本文档的搭建",
+            collapsable: false,
+            sidebarDepth: 0,
+            children: [
+                "blog/blog-build-vuepress.md",
+                "blog/blog-build-ci.md",
+                "blog/blog-build-cd.md",
+                "blog/blog-build-ssl.md"
+            ]
+        }
+    ];
+}
--- readme.md
+++ readme.md
@@ -1,13 +1,13 @@
 ---
 home: true
-heroImage: /images/index-read.gif
-actionText: 快速开始 →
-actionLink: /md/java/basic/java-basic-oop.md
+heroImage: /images/index-read.png
+actionText: 生存六法则 →
+actionLink: /md/think/basic/think.md
 features:
 - title: 夯实基础
   details: 不积跬步无以至千里, 仰望星空还需脚踏实地
-- title: 构建体系
-  details: 告别碎片化学习，帮助你构筑你自己的知识体系
+- title: 什么是思考——不知道
+  details: 多数人为了逃避真正的思考愿意做任何事情————王兴
 - title: 全栈开发
   details: 以Java开发为背景，全栈开发，DevOps
   footer: © 2023-present kevin
